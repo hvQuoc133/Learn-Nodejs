c0073fa3859c708bf58e9d9fef07e61b3a9323b0
hovanquoc <songtulap001.com>
2025-01-02 18:01:57 +0700

    connection database

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webRouter = require('./routes/web');
-
+const connection = require('./config/database');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,6 +13,16 @@ configViewEngine(app);
 
 //routes init
 app.use('/', webRouter);
+
+//
+
+//simple query
+connection.query(
+  'SELECT * FROM Users u',
+   function(err, results, fields) {
+    console.log(">>> Check results: ", results);
+});
+
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
